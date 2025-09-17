2474ba892329129eb2282fd7f9bf5b78ee12a467
HAK1580 <aliofficial1580630@gmail.com>
2025-09-18 01:27:24 +0500

    this is good

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -7,7 +7,7 @@ const Hamburger = () => {
   return (
     <div  className='menu overflow-hidden md:hidden  animate-ham  max-w-[87%] border-1 rounded-[2px]  border-[rgb(255,255,255,0.15)] min-h-[85vh] relative top-[clamp(-1rem,0.3rem,0.5rem)]  '>
        <div className="nav"> 
-            <ul className='flex font-loop tracking-widest  flex-col gap-[2.2rem] text-[clamp(1.17rem,3vw,2.5rem)]  py-[1.5rem] px-[0.9rem] '>
+            <ul className='flex font-loop tracking-widest  flex-col gap-[2.2rem] text-[clamp(1rem,3vw,2.5rem)]  py-[1.5rem] px-[0.9rem] '>
                 <li> <a href="/">Home</a></li>
                 <li> <a href="/">All Collections</a> </li>
                 <li> <a href="/">ABOUT US</a> </li>
