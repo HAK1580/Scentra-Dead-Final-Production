bc8be25db90fd457ba57bb8a3a3635118b45d6b0
HAK1580 <aliofficial1580630@gmail.com>
2025-09-17 19:24:45 +0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './index.css'
 import React from 'react'
-import HomePage from './Pages/HomePage'
+import HomePage from "./Pages/HomePage.jsx"
 
 function App() {
 
