ab1b135b74314100900e996f0df90c613d87ff74
HAK1580 <aliofficial1580630@gmail.com>
2025-09-18 22:27:21 +0500

    done done done responsive

--- src/components/SliderHero.jsx
+++ src/components/SliderHero.jsx
@@ -7,7 +7,7 @@
    const imgs=[
      "/IMGS/Banners/1.jpg",   
     "/IMGS/Banners/2.jpg",
-     "/IMGS/Banners/4.jpg"
+     "/IMGS/Banners/6.jpg"
    ]
 
    const [current, setCurrent] = useState(0);
