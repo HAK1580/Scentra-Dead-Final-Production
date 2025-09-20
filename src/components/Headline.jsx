9e8bd57a278538a30d2485a9b6aac191fe87df59
HAK1580 <aliofficial1580630@gmail.com>
2025-09-20 18:13:57 +0500

    done dona done

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Headline = () => {
   return (
     <div>
-     <div className="headline max-w-full font-bold  tracking-wide font-tree text-[clamp(0.61rem,1.33vw,2rem)] py-[clamp(0.2rem,0.35rem,0.5rem)] px-0  mb-8.5  bg-[rgba(221,179,52,0.86)] text-[#000000] ">
-       <div className="headlinetxt font-bold max-w-full flex justify-center items-center ">At Scentra we provide free delivery | 100% return back policy </div>
+     <div className="headline  py-3   text-[clamp(0.73rem,1.33vw,2rem)]  max-w-full sm:font-bold  tracking-wide font-tree sm:text-[clamp(0.61rem,1.33vw,2rem)] sm:py-[clamp(0.2rem,0.35rem,0.5rem)] sm:px-4  mb-8.5  bg-[rgba(221,179,52,0.86)] text-[#000000] ">
+       <div className="headlinetxt font-extrabold  sm:font-bold max-w-full flex justify-center items-center ">At Scentra we provide free delivery | 100% return back policy </div>
     </div> 
 
     </div>
