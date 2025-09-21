cd1adf7bd7fcca59adc89102e41ed37928c7d693
HAK1580 <aliofficial1580630@gmail.com>
2025-09-21 14:36:48 +0500

    done dona done

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -4,7 +4,7 @@ const Headline = () => {
   return (
     <div>
      <div className="headline  py-2.5   text-[clamp(0.73rem,1.33vw,2rem)]  max-w-full sm:font-bold  tracking-wide font-tree sm:text-[clamp(0.61rem,1.33vw,2rem)] sm:py-[clamp(0.2rem,0.35rem,0.5rem)] sm:px-4  mb-8.5  bg-[rgba(221,179,52,0.86)] text-[#000000] ">
-       <div className="headlinetxt font-extrabold  sm:font-bold max-w-full flex justify-center items-center ">At Scentra we provide free delivery | 100% return back policy </div>
+       <div className="headlinetxt tree  font-extrabold  sm:font-bold max-w-full flex justify-center items-center ">At Scentra we provide free delivery | 100% return back policy </div>
     </div> 
 
     </div>
