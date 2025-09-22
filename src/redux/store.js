0983d2c86941b88ca7cfd28a8747311b6cedb160
HAK1580 <aliofficial1580630@gmail.com>
2025-09-22 12:46:16 +0500

    fixed the responsive issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import uireducer from "./ui_states/uiSlice"
+import cartReducer from "./cart/cartSlice"
 
 export const store = configureStore({
   reducer: {
-    ui:uireducer
+    ui:uireducer,
+    cart:cartReducer,
   },
 })
