97303440b85c182376e78568d1e05a99710f10f7
HAK1580 <aliofficial1580630@gmail.com>
2025-09-22 15:08:30 +0500

    fixed the responsive issues

--- src/pages/ShoppingPage.jsx
+++ src/pages/ShoppingPage.jsx
@@ -207,19 +207,19 @@ const search = useSelector((state) => state.ui.searchOpen);
         <h1 className='text-center loop  text-[5.7vw]  tracking-wide  my-8  text-[#e8d5a9] sm:text-[3.5vw] '> VIEW All COLLECTIONS   </h1>
 
            
-     <div className="shop grid grid-cols-2 md:grid-cols-3   lg:grid-cols-4 max-w-[92%]   md:max-w-[94%] mx-auto gap-[2.5vw]    sm:gap-[1.8vw] ">
+     <div className="shop grid grid-cols-2 md:grid-cols-3   lg:grid-cols-4 max-w-[92%]   md:max-w-[94%] mx-auto gap-[3vw]    sm:gap-[1.8vw] ">
         {product_info.map((product)=>
 
         <div key={product.id} className="product-info h-fit flex-col     rounded-[8px] border-1   max-w-full   border-gray-200">
         <div className="product-image m-0   sm:m-4 object-contain relative flex justify-center  items-center cursor-pointer">
           <img className='border     border-[#ffffffaf] rounded-[8px] max-w-full object-contain  sm:h-full' src={product.image} alt="" />
-          <img className='absolute max-w-full  rounded-[3px] top-[0.1rem] right-[0.05rem] ' width={70} src="/IMGS/icons/sale.svg " alt="" />
+          <img className='absolute w-[50px] sm:w-[68px] sm:max-w-full  rounded-[3px] top-[0.05rem] right-[0.05rem] ' width={70} src="/IMGS/icons/sale.svg " alt="" />
         </div>
-        <div  className="product-title h-[5vh] sm:h-fit   flex justify-start loop  mx-auto  w-[90%]  ">
-          <h2   className=' mt-5 text-[4.25vw] md:text-[1.9vw]'> {product.title} </h2>  
+        <div  className="product-title mb-3.5 sm:mb-2  h-[5vh] sm:h-fit   flex justify-start loop  mx-auto  w-[90%]  ">
+          <h2   className='mb-1 mt-5 text-[4.25vw] md:text-[1.9vw]'> {product.title} </h2>  
 
         </div>
-          <div className="para py-1.5 text-[3.2vw] sm:text-[1.2vw]  sm:py-1  sm:h-full h-[5vh] line-clamp-4 flex justify-start my-0.5  mx-auto w-[90%] ">
+          <div className="para py-1.5 text-[2.9vw]  sm:text-[3vw] md:text-[1.2vw]  sm:py-1  sm:h-full h-[4vh] line-clamp-4 flex justify-start my-2  mx-auto w-[90%] ">
             <h3 className='italic font-extralight' > {product.desc}   </h3>
           </div>
           <div className="price py-0 sm:p-1 my-1 sm:my-2   ">
@@ -229,7 +229,7 @@ const search = useSelector((state) => state.ui.searchOpen);
           </div>
 
         <div   className="add-to-cart-btn ">
-         <button    key={product.id}   className='w-full add-to-cart-hover rounded-[4px]  sm:rounded-[2px]  cursor-pointer py-1 hover:bg-[rgb(186,149,63)] hover:text-black  border-1 rouned-[6px] sm:text-[2.5vw]  lg:text-[1.65vw]' >Add to Cart</button>
+         <button    key={product.id}   className='w-full add-to-cart-hover rounded-[4px]  sm:rounded-[2px]  cursor-pointer py-1.5 sm:py-2  hover:bg-[rgb(186,149,63)] hover:text-black  border-1 rouned-[6px] sm:text-[2.5vw]  lg:text-[1.65vw]' >Add to Cart</button>
         </div>
           
         </div>
