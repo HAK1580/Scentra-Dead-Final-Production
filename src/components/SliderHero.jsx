a4c06dbc8279760e31c3a4f254e207605de37b22
HAK1580 <aliofficial1580630@gmail.com>
2025-09-23 23:54:55 +0500

    responsive cart

--- src/components/SliderHero.jsx
+++ src/components/SliderHero.jsx
@@ -53,7 +53,7 @@
            
               ">
             
-             <button  className="bg-[#000000d0]  lg:tracking-wide  px-[clamp(1.2rem,3.5vw,3.1rem)]  text-[clamp(1.15rem,2.5vw,3rem)] py-[clamp(0.4rem,0.3vw,1.5rem)] border-1 border-[#d4af3767] rounded-[8px] text-white cursor-pointer font-[700] z-100 tracking-widest 
+             <button  className="bg-[#000000d0]  lg:tracking-wide  px-[clamp(1.2rem,3.5vw,3.1rem)]  text-[clamp(1.15rem,2.5vw,3rem)] py-[clamp(0.4rem,0.3vw,1.5rem)] border-1 border-[#d4af3767] rounded-[8px] text-white cursor-pointer font-[500] z-100 tracking-widest 
               lg:rounded-[13px]
               font-inter 
               lg:font-light sm:font-light hover:font-[500]
