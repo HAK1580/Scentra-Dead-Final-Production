3a14dec8fae3f64a79aad466d0f0c6bd67ee6827
HAK1580 <aliofficial1580630@gmail.com>
2025-09-24 01:39:10 +0500

    responsive cart

--- src/components/MiddleHeadline.jsx
+++ src/components/MiddleHeadline.jsx
@@ -1,49 +1,8 @@
-import React from 'react'
-
-const MiddleHeadline = ({hero}) => {
-  return (
-    <>
-    <div className={`headline-2  z-100  
-      my-[75vh]  md:my-[100vh]  lg:my-[150vh]
-       overflow-hidden  ${hero?'hidden':""}  max-w-full `}>
-      <div className="headlinetxt2 max-w-full flex-nowrap flex shrink-0 my  gap-[10vw] text-[clamp(1.1rem,4vw,2rem)]   ">
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-        <h3>Scentra</h3>
-      </div>
-
-
-
-    </div>
-    
-  
-  
-
-
-    
-    </>
-  )
-}
-
-export default MiddleHeadline
+<div className={`headline-2 z-100 my-[75vh] md:my-[100vh] lg:my-[150vh] ${hero ? 'hidden' : ""} w-full overflow-hidden`}>
+  <div className="headlinetxt2 flex flex-nowrap gap-[10vw] text-[clamp(1.1rem,4vw,2rem)]">
+    {/* Repeat text */}
+    {Array(25).fill("Scentra").map((txt, i) => (
+      <h3 key={i} className="shrink-0">{txt}</h3>
+    ))}
+  </div>
+</div>
