e73e4ad725b45e728420e94c73f17bf6838cdf38
HAK1580 <aliofficial1580630@gmail.com>
2025-09-24 01:44:39 +0500

    responsive cart

--- src/components/MiddleHeadline.jsx
+++ src/components/MiddleHeadline.jsx
@@ -1,4 +1,9 @@
-<div className={`headline-2 z-100 my-[75vh] md:my-[100vh] lg:my-[150vh] ${hero ? 'hidden' : ""} w-full overflow-hidden`}>
+import React from 'react'
+
+const MiddleHeadline = ({hero}) => {
+  return (
+    <>
+  <div className={`headline-2 z-100 my-[75vh] md:my-[100vh] lg:my-[150vh] ${hero ? 'hidden' : ""} w-full overflow-hidden`}>
   <div className="headlinetxt2 flex flex-nowrap gap-[10vw] text-[clamp(1.1rem,4vw,2rem)]">
     {/* Repeat text */}
     {Array(25).fill("Scentra").map((txt, i) => (
@@ -6,3 +11,13 @@
     ))}
   </div>
 </div>
+
+  
+
+
+    
+    </>
+  )
+}
+
+export default MiddleHeadline
