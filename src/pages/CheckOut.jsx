c6abf149baea6b154ec76085efb7897435a2475f
HAK1580 <aliofficial1580630@gmail.com>
2025-09-26 01:46:05 +0500

    almost done the check out page

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -17,34 +17,34 @@ const CheckOut = () => {
     return (
 
         <div >
-            <div className="center-logo relative  border  w-[100%]  mx-auto  py-5  
+            <div className="center-logo relative   w-[100%]  mx-auto  py-5  
         flex justify-center items-center 
          ">
                 <img className='w-38 md:w-42 lg:w-55
             sm:left-30  lg:left-10
             lg:top-3
-            absolute top-1 left-8 md:left-40  max-[500px]:w-28
+            absolute top-1 left-4 md:left-40  max-[500px]:w-29
             
             ' src="IMGS/logo/logo-fotor-bg-remover-2025080510633.webp" alt="" />
-                <h1 className='text-[10vw] sm:text-[8vw] lg:text-[5rem]
+                <h1 className='text-[15vw] sm:text-[8vw] lg:text-[5rem]
             loop font-[100] cursor-pointer
-            xl:text-[6rem]
+            xl:text-[6rem] tracking-wide
             text-[#e8d5a9]' >Scentra</h1>
             </div>
 
             <div className="checkout-form-div my-1 ">
-                <h1 className=' text-[8vw] mb-4 sm:text-[3.4vw] tracking-wider  p-3 font-light '> CheckOut </h1>
+                <h1 className=' text-[9vw] tree   mb-4 sm:text-[3.4vw] tracking-wider text-[rgb(232,213,169)] p-3  '> Checkout </h1>
 
                 <form className='font-inter tracking-normal flex flex-col gap-12 justify-center  max-w-[94%] mx-auto' onSubmit={handleSubmit(onSubmit)} >
 
                     <div className="contact">
-                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-1 tracking-wide mb-2 ' >Contact</h1>
+                        <h1 className='text-[6vw] font-light  sm:text-[2.7vw] ml-1 tracking-wide mb-2 text-[rgb(232,213,169)] ' >Contact</h1>
                         <input {...register("phone", { required: "Enter phone number", pattern: { value: /^(?:\+92|0)?3[0-9]{9}$/, message: "Enter valid phone number" } })} className={`${errors.phone ? "outline-red-500 outline-3" : "outline-0 outline-black"} ml-1 mt-3  w-[94%] p-3 rounded-[4px]  bg-white text-black`} type="text" placeholder='Mobile phone number' />
                         {errors.phone && <p className='text-[#ff0000cd]  mt-3  ml-1'>{errors.phone.message}</p>}
                     </div>
 
                     <div className="delivery">
-                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-1 tracking-wide mb-2 '>Delivery</h1>
+                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-1 font-light  tracking-wide mb-2 text-[rgb(232,213,169)] '>Delivery</h1>
 
                         <input {...register("first_name", { required: "Enter a first name", pattern: { value: /^[A-Za-z][A-Za-z\s'-]{1,29}$/, message: "Enter valid first name", } })} className={`${errors.first_name ? "outline-red-500 outline-3" : "outline-0 outline-black"} ml-1   w-[94%] p-3 rounded-[4px]  bg-white text-black`} type="text" placeholder='First name' />
                         {errors.first_name && <p className='text-[#ff0000cd] mt-3  ml-1'>{errors.first_name.message}</p>}
@@ -66,13 +66,13 @@ const CheckOut = () => {
 
                     </div>
                     <div className="payment">
-                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-2 tracking-wide mb-2 '>
+                        <h1 className='text-[6vw] sm:text-[2.7vw] font-light ml-2 tracking-wide mb-2 text-[rgb(232,213,169)]  '>
                             Payment </h1>
                         <label className="flex mt-4 ml-2  text-black bg-gray-200 py-3.5 px-2  items-center gap-2">
                             <input
                                 type="radio"
                                 value="cash_on_delivery"
-                                defaultChecked
+                            
                                 {...register("paymentMethod", { required: true })}
                             />
                             Cash on Delivery
@@ -80,7 +80,7 @@ const CheckOut = () => {
                     </div>
 
                     <div className="billing-address">
-                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-2 tracking-wide mb-2 '> Billing address  </h1>
+                        <h1 className='text-[6vw] sm:text-[2.7vw] ml-2  font-light tracking-wide mb-2 text-[rgb(232,213,169)]  '> Billing address  </h1>
                       <label className='flex border rounded-[6px] px-2 text-[4vw] text-black bg-gray-200 py-3.5  ml-2 mt-4 gap-2'  >
                        <input {...register("address")}   type="radio" name='billing_address'  value={"same as shipping add"}   />
                           Same as Shipping Address 
