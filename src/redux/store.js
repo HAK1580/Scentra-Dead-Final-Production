bc290623691809c03022742ccb9fefaef593c778
HAK1580 <aliofficial1580630@gmail.com>
2025-09-28 17:36:29 +0500

    improved ux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
 import uireducer from "./ui_states/uiSlice"
 import cartReducer from "./cart/cartSlice"
-import ProductReducer, { productSlice } from './products/productSlice'
+import ProductReducer from './products/productSlice'
+
+// Load cart from localStorage if it exists
+const savedCart = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : undefined
 
 export const store = configureStore({
   reducer: {
-    ui:uireducer,
-    cart:cartReducer,
-    products:ProductReducer,
+    ui: uireducer,
+    cart: cartReducer,
+    products: ProductReducer,
   },
+  preloadedState: {
+    cart: savedCart ? { items: savedCart.items } : undefined,
+  },
+})
+
+// Subscribe to store changes to save cart to localStorage
+store.subscribe(() => {
+  const state = store.getState()
+  localStorage.setItem('cart', JSON.stringify(state.cart))
 })
