2a70e2db3bbc7b4d72a4762e2e9b6bbe6ea3164b
HAK1580 <aliofficial1580630@gmail.com>
2025-09-28 17:24:02 +0500

    improved ux

--- src/components/small_ui_com/OrderConfirmation.jsx
+++ src/components/small_ui_com/OrderConfirmation.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 const OrderConfirmation = ({orderconfirm,setOrderconfirm}) => {
+
+
   return (
-    <div className={`${orderconfirm?"opacity-100":"opacity-0"}  flex justify-center  `}>
+    <div className={` flex justify-center  `}>
 
         <div className={` border-2 border-transparent
-         bg-green-700 py-2 fixed lg:top-0   
+         bg-green-700 py-5 fixed lg:top-0   
          lg:max-w-[40%] 
          w-[99%] my-0
          mx-auto lg:my-20 text-white 
           z-1000
           
-        `}>          
-             <h1 className='lg:text-[2vw]
-
-             pl-3
-             ' 
-              > Order Confirmation  </h1>
-              <h2 className='lg:text-[1.5vw] my-2 pl-4 '> Thank You for placing Order ! We will contact you as soon as possible... </h2>
-
+        `}>      
+          <div className="order-confirm-img  items-center flex ">
+            <img className='w-8'  src="IMGS/icons/circletick.svg" alt="" />
+             <h1 className='lg:text-[2vw] max-[400px]:text-[6vw] pl-2' > Order Confirmation  </h1>
+          </div>    
+            <h2 className='lg:text-[1.5vw] my-2 pl-4 '> Thank You for placing Order ! We will contact you as soon as possible... </h2>
         </div>
     </div>
   )
