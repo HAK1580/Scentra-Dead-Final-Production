d3b83db062053a34f9e60af3a2c38cce9537af0e
HAK1580 <aliofficial1580630@gmail.com>
2025-09-30 17:22:24 +0500

    yes making it final now

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -7,7 +7,7 @@ const Hamburger = () => {
   return (
     <div  className='menu overflow-hidden md:hidden  animate-ham  max-w-[87%] border-1 rounded-[2px]  border-[rgb(255,255,255,0.15)] min-h-[85vh] relative top-[clamp(-1rem,0.3rem,0.5rem)]  '>
        <div className="nav"> 
-            <ul className='flex font-loop tracking-widest  flex-col gap-[2.3rem] text-[clamp(1.10rem,3vw,2.5rem)]  py-[1.5rem] px-[0.9rem] '>
+            <ul className='flex  ham-nav font-loop tracking-widest  flex-col gap-[2.3rem] text-[clamp(1.10rem,3vw,2.5rem)]  py-[1.5rem] px-[0.9rem] '>
                 <li> <a href="/">Home</a></li>
                 <li> <a href="/shopping">All Collections</a> </li>
                 <li> <a href="/">ABOUT US</a> </li>
@@ -17,8 +17,8 @@ const Hamburger = () => {
               
         </div>
           <div className="follow-us my-12">
-             <h1 className='p-3 ml-2 text-[#e8d5a9e7] text-[4.9vw]'>LOG IN </h1>
-                        <div className="social-media-icons py-6 gap-7 flex justify-center ">
+             <h1 className='p-3 ml-2 text-[#e8d5a9e7] text-[4.7vw]'>LOG IN </h1>
+                      <div className="social-media-icons py-6 gap-7 flex justify-center ">
             <img className='w-6'   src="IMGS/icons/ticktok.svg" alt="" />
             <img className='w-6'   src="IMGS/icons/fb.svg" alt="" />
             <img className='w-6'   src="IMGS/icons/youtube.svg" alt="" />
