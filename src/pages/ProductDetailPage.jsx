1ac072a731eb6f9b5681642678336a1a6c901800
HAK1580 <aliofficial1580630@gmail.com>
2025-10-02 15:42:24 +0500

    finally goal achieved

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -38,20 +38,20 @@ const ProductDetailPage = () => {
       <div className="product-info-div flex flex-col
       gap-2 overflow-x-hidden
       ">
-        <img className='object-contain w-full h-auto ' src={`/${product.image}`} alt="" />
+        <img className='object-contain w-[95%] mx-auto h-auto ' src={`/${product.image}`} alt="" />
         <div className="product-description mt-6 ">
-          <h1 className='flex justify-center italic sm:text-[3.7vw]  arimo text-[4.5vw]'> {product.desc} </h1>
-          <h1 className='flex justify-center my-3 rubik tracking-wider  sm:text-[7vw] text-[8.2vw]'> {product.title} </h1>
+          <h1 className='flex justify-center italic sm:text-[3.7vw]  arimo text-[3.5vw]'> {product.desc} </h1>
+          <h1 className='flex justify-center my-1 rubik tracking-wider  sm:text-[7vw] text-[7.2vw]'> {product.title} </h1>
         </div>
         <div className="product-price justify-start items-center   w-[60%] mx-auto gap-6  flex ">
           <h1 className='flex justify-center sm:text-[4vw]  italic line-through decoration-red-500 decoration-2 arimo text-[5.5vw]'> Rs. {product.oldprice} </h1>
-          <h1 className='flex justify-center sm:text-[4vw]  italic  arimo text-[6.5vw]'>Rs.{product.price} </h1>
+          <h1 className='flex justify-center sm:text-[4vw]  italic  arimo text-[5vw]'>Rs.{product.price} </h1>
         </div>
 
        <div className="add-to-cartt  flex flex-col gap-3 mt-1">
-       <button onClick={addtoCart}  className='border-1 pt-cart text-[5.7vw] font-inter   text-center text-black bg-[#d4af37]  lg:bg-black   w-[100%]   mx-auto sm:text-[4.5vw] cursor-pointer font-extrabold   py-3  lg:hover:bg-[#d4af37ad] hover:text-[rgb(33,33,33)]" sm:hover:text-black         
+       <button onClick={addtoCart}  className='border-1 pt-cart  text-[5.7vw] font-inter   text-center text-black bg-[#d4af37]  lg:bg-black   w-[97%]   mx-auto sm:text-[4.5vw] cursor-pointer font-extrabold   py-3  lg:hover:bg-[#d4af37ad] hover:text-[rgb(33,33,33)]" sm:hover:text-black         
         '> Add to Cart</button>
-        <button onClick={gotocheckout}  className='border-1 pt-cart text-[5.7vw] font-inter   flex justify-center items-center     w-[100%]   mx-auto sm:text-[4.5vw] cursor-pointer font-extrabold   py-3  lg:hover:bg-[#d4af37ad]        
+        <button onClick={gotocheckout}  className='border-1 pt-cart text-[5.7vw] font-inter   flex justify-center items-center     w-[97%]   mx-auto sm:text-[4.5vw] cursor-pointer font-extrabold   py-3  lg:hover:bg-[#d4af37ad]        
          '> Check Out</button>
     
        </div>
